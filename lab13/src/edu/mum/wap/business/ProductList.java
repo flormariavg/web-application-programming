3f33a3163304e27f0927145481fe9d6ec0b37d61
DELL <carlos.a.hernandez.ospina@gmail.com>
2019-03-15 07:44:39 -0500

    Servlet para ProductList

---
+++ lab13/src/edu/mum/wap/business/ProductList.java
@@ -0,0 +1,34 @@
+package edu.mum.wap.business;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import edu.mum.wap.data.Product;
+import edu.mum.wap.model.Item;
+
+@WebServlet("/ProductList")
+public class ProductList extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		List<Item> products = (List<Item>) request;
+		for (Item item : products) {
+			System.out.println(item);
+		}
+	
+		request.setAttribute("products", products);
+		request.getRequestDispatcher("checkout.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+	}
+
+}
